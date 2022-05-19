a0d8ce65614a237ab6506b2fd4e0e8b3da89ea52
Gianluca Beltran <gianlucabeltran@gmail.com>
2022-05-19 17:18:44 -0500

    Implemented ccp in react

--- react_ccp/src/App.js
+++ react_ccp/src/App.js
@@ -2,56 +2,17 @@ import './App.css';
 import React from 'react';
 import ConnectListener from './aws-connect-listener';
 import isBrowserCompatible from './compatibility';
+import CCP from './CCP';
 
 function App() {
 
-  const cont = React.createRef();
-
-  if (!isBrowserCompatible()) {
-    return (
-      <div className='App'>
-        "Sorry, browser not supported. Please switch to one of the three latest versions of Chrome or Firefox."
-      </div>
-    )
-  }
-
-  else {
-    const connectListener = new ConnectListener(cont.current, "https://ac-datamatics.my.connect.aws/ccp-v2");
-
-    connectListener.onCallConnectingOrIncoming = (contact) => {
-      alert("You have an incoming call ☎️");
-    }
-    connectListener.onCallMissed = () => {
-      alert("Missed call 📞")
-    }
-    connectListener.onCallConnected = (contact) => {
-      const connection = contact.getAgentConnection();
-      alert("Connected to call. ID of connection is " + connection.getConnectionId());
-    }
-    connectListener.onCallError = () => {
-      alert("error");
-    }
-
-    connectListener.onCallPending = () => {
-      alert("Pending")
-    }
-
-    connectListener.onCallEnded = () => {
-      alert("Ended")
-    }
-    connectListener.onCallDestroy = () => {
-      alert("Call is destroyed");
-    }
-    connectListener.onCallACW = () => {
-      alert("ACW");
-    }
-
     return (
       <div className="App">
-        <div ref={cont}></div>
+        <div id={"ccp"}>
+          {isBrowserCompatible() && <CCP instanceURL={"https://ac-datamatics.my.connect.aws/ccp-v2"} />}
+        </div>
       </div>
     );
   }
-}
 
 export default App;
