714766e34fdc23d5e87d967cae0780170cdb753d
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2022-05-22 21:28:30 -0500

    Added callbacks to react model (needs testing)

--- react_ccp/src/App.js
+++ react_ccp/src/App.js
@@ -4,12 +4,60 @@ import isBrowserCompatible from './compatibility';
 import CCP from './CCP';
 
 function App() {
-
+    // Used to call methods of the object
+    const ccp = React.createRef();
     return (
       <div className="App">
-        <div id={"ccp"}>
-          {isBrowserCompatible() && <CCP instanceURL={"https://ac-datamatics.my.connect.aws/ccp-v2"} />}
-        </div>
+          {isBrowserCompatible() && <CCP 
+            instanceURL={"https://ac-datamatics.my.connect.aws/ccp-v2"} 
+            ref={ccp}
+            onInstanceConnected={() => {
+              // Called on instance init, when an agent logs in
+            }}
+            onInstanceTerminated={() => {
+              // Called on instance termination, when an agent logs out
+            }}
+            onAgent={(agent)=>{
+              // Called after initialization, when an agent is assigned to the ccp
+            }}
+            onAgentStateChange={(state) => {
+              // Called when the agent's state changes (ie, they are online/offline, in a call or on acw)
+            }}
+            onContact={(contact) => {
+              // Called when new info of a contact is received
+            }}
+            onIncomingContact={(contact) => {
+              // Called when there is an incoming contact (eg, the phone is ringing)
+              // Here the option to answer and start recording should be shown
+            }}
+            onPendingContact={(contact) => {
+              // Called before the connectedContact event. The contact is pending
+              // https://github.com/amazon-connect/amazon-connect-streams/blob/master/Documentation.md#contactonpending
+            }}
+            onMissedContact={(contact) => {
+              // Called When the contact was missed
+            }}
+            onConnectedContact={(contact) => {
+              // Called when the contact is connected (the call has started)
+            }}
+            onEndedContact={(contact) => {
+              // Called when either party hangs up
+            }}
+            onErrorContact={(contact) => {
+              // Called in case of error
+            }}
+            onRejectedContact={(contact) => {
+              // Called if the contact was rejected or declined
+            }}
+            onDestroyContact={(contact) => {
+              // Called after acw, when the agent closes the communication with the contact
+              // Here, the stored recording should be uploaded to S3
+              // Here, a lambda must be called to insert the recording's data into the database
+            }}
+            onAfterCallWork={(contact) => {
+              // Called after the call has ended but the agent is still working
+            }}
+          />}
       </div>
     );
   }
--- react_ccp/src/CCP.js
+++ react_ccp/src/CCP.js
@@ -6,48 +6,140 @@ import isBrowserCompatible from "./compatibility";
 class CCP extends Component {
     constructor(props) {
         super(props);
+        this.containerDiv = React.createRef();
         this.instanceURL = props.instanceURL
+        this.state = {
+            initialized: false
+        }
+        this.__loginWindow;
     }
 
     componentDidMount() {
         if (!isBrowserCompatible) {
-            document.getElementById("ccp").innerText="Sorry, browser not supported. Please switch to one of the three latest versions of Chrome or Firefox."
+            this.containerDiv.current.innerHTML = "Sorry, browser not supported. Please switch to one of the three latest versions of Chrome or Firefox."
+            return
         }
-        else {
-            connect.core.initCCP(document.getElementById("ccp"), {
-                // CONNECT CONFIG
-                ccpUrl: this.instanceURL, // Required
-                region: "us-east-1", // Region of the instance
-                ccpAckTimeout: 5000, //optional, defaults to 3000 (ms)
-                ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
-                ccpLoadTimeout: 10000, //optional, defaults to 5000 (ms)
-                // LOGIN
-                loginPopup: true, // Show a popup window to authenticate
-                loginPopupAutoClose: true, // Auto close login popup after auth
-                loginOptions: {
-                    autoClose: true,
-                    height: 400,
-                    width: 400,
-                    top: 0,
-                    left: 0
-                },
-                // PHONE OPTIONS
-                softphone: {
-                    allowFramedSoftphone: true,   // optional, defaults to false
-                    disableRingtone: false,       // optional, defaults to false
-                    ringtoneUrl: false // optional, defaults to CCP’s default ringtone if a falsy value is set
-                },
-                pageOptions: { //optional
-                    enableAudioDeviceSettings: false, //optional, defaults to 'false'
-                    enablePhoneTypeSettings: false //optional, defaults to 'true' 
-                },
-            })
-        }
+        connect.core.initCCP(this.containerDiv.current, {
+            // CONNECT CONFIG
+            ccpUrl: this.instanceURL, // Required
+            region: "us-east-1", // Region of the instance
+            ccpAckTimeout: 5000, //optional, defaults to 3000 (ms)
+            ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
+            ccpLoadTimeout: 10000, //optional, defaults to 5000 (ms)
+            // LOGIN
+            loginPopup: false, // Show a popup window to authenticate
+            loginPopupAutoClose: false, // Auto close login popup after auth
+            loginOptions: {
+                autoClose: false,
+                height: 400,
+                width: 400,
+                top: 0,
+                left: 0
+            },
+            // PHONE OPTIONS
+            softphone: {
+                allowFramedSoftphone: true,   // optional, defaults to false
+                disableRingtone: false,       // optional, defaults to false
+                ringtoneUrl: false // optional, defaults to CCP’s default ringtone if a falsy value is set
+            },
+            pageOptions: { //optional
+                enableAudioDeviceSettings: false, //optional, defaults to 'false'
+                enablePhoneTypeSettings: false //optional, defaults to 'true' 
+            },
+        });
+        // On ccp instance terminated
+        connect.core.getEventBus().subscribe(connect.EventType.TERMINATED, () => {
+            this.setState({ initialized: false });
+            // Callback
+            this.props.onInstanceTerminated?.();
+        });
+        // On connected to ccp
+        connect.core.getEventBus().subscribe(connect.EventType.UPDATE_CONNECTED_CCPS, () => {
+            this.setState({ initialized: true });
+            // Close login window
+            this.__loginWindow?.close();
+            // Callback
+            this.props.onInstanceConnected?.();
+            // Listen to agents 
+            connect.agent(agent => {
+                // Store agent
+                this.agent = agent;
+                // Callback
+                this.props.onAgent?.(agent);
+                // Listen to agent changes
+                agent.onStateChange(state => {
+                    // Avoid duplicate events
+                    if (state.newState == state.oldState) return;
+                    // Callback
+                    this.props.onAgentStateChange?.(state);
+                });
+            });
+            // Listen to contacts
+            connect.contact(contact => {
+                // Callback
+                this.props.onContact?.(contact);
+                // Store previous state
+                let previousState = contact.getState().type;
+                // Listen to contact changes
+                contact.onRefresh(contact => {
+                    if (contact.getState().type == previousState) return;
+                    previousState = contact.getState().type;
+                    switch (previousState) {
+                        case connect.ContactStateType.INCOMING:
+                        case connect.ContactStateType.CONNECTING:
+                            return this.props.onIncomingContact?.(contact);
+                        case connect.ContactStateType.PENDING:
+                            return this.props.onPendingContact?.(contact);
+                        case connect.ContactStateType.MISSED:
+                            return this.props.onMissedContact?.(contact);
+                        case connect.ContactStateType.CONNECTED:
+                            return this.props.onConnectedContact?.(contact);
+                        case connect.ContactStateType.ENDED:
+                            return this.props.onEndedContact?.(contact);
+                        case connect.ContactStateType.ERROR:
+                            return this.props.onErrorContact?.(contact);
+                        case connect.ContactStateType.REJECTED:
+                            return this.props.onRejectedContact?.(contact);
+                    }
+                })
+                // Listen to contact events
+                contact.onACW(() => {
+                    if (previousState == "after-call-work") return;
+                    previousState = "after-call-work";
+                    this.props.onAfterCallWork?.();
+                });
+                contact.onDestroy(() => {
+                    if (previousState == "destroy") return;
+                    previousState = "destroy";
+                    this.props.onDestroyContact?.(contact);
+                });
+            });
+
+        });
+
+    }
+
+    openLoginPopup() {
+        // Remove from localstorage the item that says the popup was shown
+        localStorage.removeItem('connectPopupManager::connect::loginPopup');
+        // Open login window
+        this.__loginWindow = window.open(this.instanceURL, "window2", "popup=1");
     }
 
     render() {
-        return <></>
+        if (!isBrowserCompatible()) return <div> This browser is not compatible </div>
+        return (
+            <div>
+                <div ref={this.containerDiv} style={{ display: this.state.initialized ? 'block' : 'none', height: '400px', width: '400px' }}></div>
+                <div style={{ display: this.state.initialized ? 'none' : 'initial' }}>
+                    <h1>Please login here</h1>
+                    <button onClick={this.openLoginPopup.bind(this)}>Login</button>
+                </div>
+                <p>+52 55 4440 5475</p>
+            </div>
+        )
     }
+
 }
 
 export default CCP
